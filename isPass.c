359db61dcaf7249c7902f1d0679f0a9b8e7eeaf7
Dit <145139482+Elite011@users.noreply.github.com>
2024-08-03 20:08:44 +0530

    Basic

---
+++ isPass.c
@@ -0,0 +1,20 @@
+// Write a program to determine whether a student has passed or failed. To pass, a 
+// student requires a total of 40% and at least 33% in each subject. Assume there 
+// are three subjects and take the marks as input from the user.
+
+#include <stdio.h>
+
+int main() {
+    float mark1, mark2, mark3, total , percentage;
+
+    printf("Enter you mark for first subject:\n");
+    scanf("%f",&mark1);
+    printf("Enter you mark for second subject:\n");
+    scanf("%f",&mark2);
+    printf("Enter you mark for third subject:\n");
+    scanf("%f",&mark3);
+
+
+    if ()    
+    return 0;
+}
