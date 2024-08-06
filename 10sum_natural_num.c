6e4d8e3317408403d3dd709a38b94085e2b75306
Dit <145139482+Elite011@users.noreply.github.com>
2024-08-06 19:59:08 +0530

    Basic

---
+++ 10sum_natural_num.c
@@ -0,0 +1,15 @@
+// Write a program to sum first ten natural numbers using while loop.
+#include <stdio.h>
+
+int main() {
+    int sum=0;
+    int num =1;
+
+    while (num<=10){
+        sum=sum+num;
+        num=num+1;
+    }
+
+    printf("The sum of the first 10 natural number is %d",sum);
+    return 0;
+}
